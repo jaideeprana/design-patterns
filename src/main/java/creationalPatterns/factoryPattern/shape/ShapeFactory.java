cf8e3dde5f11e566260356e0299b7e16660bd49e
Jaideep Kumar Rana <jaideepk@thoughtworks.com>
2020-03-31 03:38:29 +0530

    JD | Add comments to shape factory pattern for better understanding

--- src/main/java/creationalPatterns/factoryPattern/shape/ShapeFactory.java
+++ src/main/java/creationalPatterns/factoryPattern/shape/ShapeFactory.java
@@ -5,6 +5,10 @@ import static creationalPatterns.factoryPattern.shape.ShapeEnum.Rectangle;
 public class ShapeFactory {
     public Shape createShape(ShapeEnum shapeName) {
         Shape shape;
+
+        /*The following code is hidden from the client code, as it need not to know how to create a shape
+        * Client code is only interested in getting the shape to work upon. And we have decoupled the creation
+        * implementation. Tomorrow if the creation implementation changes we only need to make changes in this file. */
         switch (shapeName) {
             case Rectangle:
                 shape = new Reactangle();
