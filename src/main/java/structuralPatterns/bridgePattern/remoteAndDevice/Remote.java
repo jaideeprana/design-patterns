59b74aff89f09bf079981d8a870a73479227112b
Jaideep <jaideepk@thoughtworks.com>
2020-11-03 22:35:31 +0530

    JD | Add bridge pattern example

---
+++ src/main/java/structuralPatterns/bridgePattern/remoteAndDevice/Remote.java
@@ -0,0 +1,13 @@
+package structuralPatterns.bridgePattern.remoteAndDevice;
+
+public interface Remote {
+  void power();
+
+  void volumeDown();
+
+  void volumeUp();
+
+  void channelDown();
+
+  void channelUp();
+}
