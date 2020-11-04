fb5fd6cc8cd6c6006ecd38d5f61e1a52363e438e
Jaideep <jaideepk@thoughtworks.com>
2020-11-04 18:22:15 +0530

    JD | Add composite pattern example

---
+++ src/main/java/structuralPatterns/composePattern/employee/Employee.java
@@ -0,0 +1,35 @@
+package structuralPatterns.composePattern.employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+  private final String name;
+  private final String dept;
+  private final int salary;
+  private final List<Employee> subordinates;
+
+  // constructor
+  public Employee(String name,String dept, int sal) {
+    this.name = name;
+    this.dept = dept;
+    this.salary = sal;
+    subordinates = new ArrayList<>();
+  }
+
+  public void add(Employee e) {
+    subordinates.add(e);
+  }
+
+  public void remove(Employee e) {
+    subordinates.remove(e);
+  }
+
+  public List<Employee> getSubordinates(){
+    return subordinates;
+  }
+
+  public String toString(){
+    return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+  }
+}
